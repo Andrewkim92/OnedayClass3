9a17d05c00625948791162b9f13bdd64f6273f86
jangy <jangy@DESKTOP-UIBTLAC>
2019-04-05 17:57:34 +0900

    homeController test 주석 commit

--- NewNew/src/main/java/com/day/one/controller/HomeController.java
+++ NewNew/src/main/java/com/day/one/controller/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	//test 주석입니다.
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
