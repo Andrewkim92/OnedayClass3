b4fc404b19c8be473f0ee8609c33e7cbe9f2a39e
Kim youngwoo <38499381+Andrewkim92@users.noreply.github.com>
2019-04-15 19:12:02 +0900

    loginController 회원가입 수정 중..

--- NewNew/src/main/java/com/day/one/controller/LoginController.java
+++ NewNew/src/main/java/com/day/one/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.day.one.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.day.one.vo.UserVO;
 
 @Controller
 @RequestMapping("/login")
@@ -11,6 +14,15 @@ public class LoginController {
 	public String register() {
 	
 		return "login/register.tiles";
+	}
+	
+	@PostMapping("/doRegister")
+	public String doRegister(UserVO vo) {
+	
+		System.out.println(vo.getId());
+		System.out.println(vo.getPassword());
+		
+		return "redirect:/";
 	}
 	
 	@RequestMapping("/findPassword")
