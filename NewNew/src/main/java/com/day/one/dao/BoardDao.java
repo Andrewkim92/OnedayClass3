d6afd4512ed9cee7d328f6038bc262c96ef175d5
Taewook <Taewook@DESKTOP-6OR93BU>
2019-04-22 23:12:08 +0900

    BoardVO, BoardDao, BoardDao.xml 수정

--- NewNew/src/main/java/com/day/one/dao/BoardDao.java
+++ NewNew/src/main/java/com/day/one/dao/BoardDao.java
@@ -1,6 +1,32 @@
 package com.day.one.dao;
 
+import com.day.one.vo.BoardVO;
+
 public interface BoardDao {
 
-	String checkId(int id);
+	// info 테이블
+	public void i_insert(BoardVO board);
+	// applicant 테이블
+	public void a_insert(BoardVO board);
+	// review 테이블
+	public void r_insert(BoardVO board);
+	
+	public BoardVO i_read(int seq_class);
+	
+	public BoardVO a_read(int seq_class);
+	
+	public BoardVO r_read(int seq_class);
+	
+	public int i_delete(int seq_class);
+	
+	public int a_delete(int seq_class);
+	
+	public int r_delete(int seq_class);
+	
+	public int i_update(BoardVO board);
+	
+	public int a_update(BoardVO board);
+	
+	public int r_update(BoardVO board);
+	
 }
--- NewNew/src/main/java/com/day/one/vo/BoardVO.java
+++ NewNew/src/main/java/com/day/one/vo/BoardVO.java
@@ -1,29 +1,73 @@
 package com.day.one.vo;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class BoardVO {
 
-	int board_id;
-	String board_title;
-	String board_content;
+//	*****info 테이블*****
 	
-	public int getBoard_id() {
-		return board_id;
-	}
-	public void setBoard_id(int board_id) {
-		this.board_id = board_id;
-	}
-	public String getBoard_title() {
-		return board_title;
-	}
-	public void setBoard_title(String board_title) {
-		this.board_title = board_title;
-	}
-	public String getBoard_content() {
-		return board_content;
-	}
-	public void setBoard_content(String board_content) {
-		this.board_content = board_content;
-	}
+	private int seq_class;
 	
+	private int userNumber;
 	
+	private String i_title;
+	
+	private String sub_title;
+	
+	private int price;
+	
+	private char approval;
+	
+	private char zip_code;
+	
+	private String address;
+	
+	private String address_detail;
+	
+	private String class_date;
+
+	private String class_time_start;
+	
+	private int class_time_during_time;
+	
+	private String file_path;
+	
+	private String inclusion;
+	
+	private String exclusion;
+	
+	private String details;
+	
+	private String preparation;
+	
+	private String i_content;
+	
+	private Date reg_date;
+	
+	private Date mod_date;
+
+//	*****applicant 테이블******
+	
+	private char deposit_check;
+	
+	private Date deposit_date;
+	
+	private char attendance_check;
+	
+	private Date a_reg_date;
+	
+//	*****review 테이블*****
+
+	private int seq_review;
+	
+	private String r_title;
+	
+	private int score;
+	
+	private String r_content;
+	
+	private Date r_reg_date;
 }
