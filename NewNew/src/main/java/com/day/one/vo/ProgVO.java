7ef2ffd421800c1cd65bdf124a5b762aa6d2cb81
YooChulKim <youway1@naver.com>
2019-06-09 18:31:48 +0900

    넘어온 프로그램 유효성검사 hibernate-validator 이용

--- NewNew/src/main/java/com/day/one/vo/ProgVO.java
+++ NewNew/src/main/java/com/day/one/vo/ProgVO.java
@@ -1,5 +1,9 @@
 package com.day.one.vo;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
@@ -7,22 +11,32 @@ public class ProgVO {
 	
 	private int progSeq;
 	
+	@NotNull
+	 @Size(min=1, max=100 , message="프로그램 이름을 입력해주세요.") 
 	private String progName;
 	
+	@NotNull
+	 @Size(min=1, max=100 , message="프로그램 대주제을 입력해주세요.") 
 	private String progBigTitle;
 	
+	@NotNull
+	 @Size(min=1, max=100 , message="프로그램 소주제을 입력해주세요.") 
 	private String progSmallTitle;
 	
+	@NotNull
+	 @Size(min=1, max=100 , message="프로그램 내용을 입력해주세요.") 
 	private String progContents;
 	
 	private String progLocation;
 	
 	private String progLikeCount;
 	
-	private String memberCode;
+	//private String memberCode;
 	
-	private String userNumber;
+	private String user_userNumber;
 	
 	private String uploadLocation;
+	
+	private String isFinished;
 	
 }
